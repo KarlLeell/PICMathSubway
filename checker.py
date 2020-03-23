cf98865d8510f0202fb0a6c83726a16e32a20d3c
Nisha Sukumar <ss11242@nyu.edu>
2020-03-23 13:22:07 -0400

    modified checker.py and added read_checker.py

--- checker.py
+++ checker.py
@@ -1,47 +1,46 @@
 import numpy as np
 import pandas as pd
 
-filename = 'FE_Checker_List.csv'
-checkers = pd.read_csv(filename)
-checkers = checkers.loc[0:30,'checker':'rdo']
+# filename = 'FE_Checker_List.csv'
+# checkers = pd.read_csv(filename)
+# checkers = checkers.loc[0:30,'checker':'rdo']
 
 class Checker:
-    def __init__(self, id, shift_start, shift_end, ft_work_status, rdo):
-        self.id = id
-        if(ft_work_status == 'Full Time'):
-            self.ft_work_status = True;
-        else:
-            self.ft_work_status = False;
-        self.avail_matrix = self.make_table(shift_start, shift_end, rdo)
-        self.rdo = self.make_rdo_table(rdo)
+  def __init__(self, name, shift_start, shift_end, ft_work_status, rdo):
+    self.name = name
+    self.ft_work_status = ft_work_status
+    self.shift_start = shift_start
+    self.shift_end = shift_end
+    self.avail_matrix = self.make_table(shift_start, shift_end, rdo)
+    self.rdo = self.make_rdo_table(rdo)
     
-    #monday - sunday
-    def make_table(self, shift_start, shift_end, rdo):
-        table = np.zeros((288, 7), dtype = int)
-        rdo_days = rdo.split(" ");
-        if(rdo_days[0] == 'FRI'):
-            skip = [4,5]
-        elif(rdo_days[0] == 'SAT'):
-            skip = [5,6]
-        else:
-            skip = [0,6]
-        for i in range(int(shift_start*12/100),int(shift_end*12/100)):
-            for j in range(0,7):
-                if(j not in skip):
-                    table[i][j] = 1
-        return table
+  #monday - sunday
+  def make_table(self, shift_start, shift_end, rdo):
+    table = np.zeros((288, 7), dtype = int)
+    rdo_days = rdo.split(" ");
+    if(rdo_days[0] == 'FRI'):
+      skip = [4,5]
+    elif(rdo_days[0] == 'SAT'):
+      skip = [5,6]
+    else:
+      skip = [0,6]
+    for i in range(int(shift_start*12/100),int(shift_end*12/100)):
+      for j in range(0,7):
+        if(j not in skip):
+          table[i][j] = 1
+    return table
     
-    def make_rdo_table(self, rdo):
-        table = np.zeros((288, 7), dtype = int)
-        rdo_days = rdo.split(" ");
-        if(rdo_days[0] == 'FRI'):
-            skip = [4,5]
-        elif(rdo_days[0] == 'SAT'):
-            skip = [5,6]
-        else:
-            skip = [0,6]
-        for i in range(0,288):
-            for j in skip:
-                table[i][j] = 1
-        return table
+  def make_rdo_table(self, rdo):
+    table = np.zeros((288, 7), dtype = int)
+    rdo_days = rdo.split(" ");
+    if(rdo_days[0] == 'FRI'):
+      skip = [4,5]
+    elif(rdo_days[0] == 'SAT'):
+      skip = [5,6]
+    else:
+      skip = [0,6]
+    for i in range(0,288):
+      for j in skip:
+          table[i][j] = 1
+    return table
     
