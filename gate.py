e347438de065e99183c12cb52a9df7539ebb96ef
jacobleeliu <44461831+jacobleeliu@users.noreply.github.com>
2020-03-30 19:35:58 -0400

    Add files via upload

--- gate.py
+++ gate.py
@@ -1,9 +1,27 @@
+#==========================================
+# Title:  Gate Class
+# Author: NYUPICMathSubwayGroup
+# Date:   2020.03.02
+# Comment:
+#==========================================
+
 import numpy as np
 from station import Station
+import math
+import constants
+import subprocess
 
 class Gate(Station):
-  def __init__(self, name = '', loc = [0,0], boro = '', routes = [], gate_id = '', task_matrix = np.zeros((24*12, 1)),
-                day = '', neighbors = [], edge_dist_tt = [], comments = ''):
+
+
+  def __repr__(self):
+    return self.name
+  def __str__(self):
+    return self.name
+
+
+  def __init__(self, name = '', loc = None, boro = '', routes = None, gate_id = '', begin_time = 0,
+                task_matrix = np.zeros((24*12, 1)), day = '', neighbors = None, edge_dist_tt = None, comments = ''):
     # inherited attribute
     #self.name_ = name
     #self.loc_ = loc
@@ -12,18 +30,51 @@ class Gate(Station):
     super().__init__(name, loc, boro, routes)
     # self attributes
     self.gate_id = gate_id
+    self.begin_time = begin_time
     self.task_matrix = task_matrix
     self.day = day
-    self.neighbors = neighbors
-    self.edge_dist_tt = edge_dist_tt
+    self.neighbors = neighbors if neighbors is not None else []
+    self.edge_dist_tt = edge_dist_tt if edge_dist_tt is not None else []
     self.comments = comments
+    self.finished = False
+
 
   # def abs_loc(self):
 
   def find_neighbors(self):
-    pass
-  def calc_edge_dist_tt(self):
-    pass
+    return self.neighbors
+
+  def calc_edge_dist_tt(self, dst_gate):
+    str1 = """ 'http://localhost:8080/otp/routers/default/plan?fromPlace="""
+    str2 = """&toPlace="""
+    str3 = """&time=1:02pm&date=3-18-2020&mode=TRANSIT,WALK&maxWalkDistance=500&arriveBy=false'"""
+    loc1 = str(self.loc)[1:-1]
+    loc2 = str(dst_gate.loc)[1:-1]
+    str4 = str1 + loc1 + str2 + loc2 + str3
+    travel_time = "curl" + str4
+    process = subprocess.run(travel_time, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+    output = process.stdout
+    output_list = output.split(":")
+    output2 = "".join(output_list)
+    output2 = output2.split(",")
+    for value in output2:
+      if "duration" in value:
+        check = value.split('"')
+        distance = int(check[-1])
+        break
+        return distance
+    #sta_loc = self.abs_loc()
+    #dst_loc = dst_gate.abs_loc()
+    #dlat = math.radians(math.radians(sta_loc[0]) - math.radians(dst_loc[0]))
+    #dlon = math.radians(math.radians(sta_loc[1]) - math.radians(dst_loc[1]))
+    #a = ((math.sin(dlat/2)) ** 2) + math.cos(math.radians(sta_loc[0])) * math.cos(math.radians(dst_loc[0])) * (math.sin(dlon/2) ** 2)
+    #c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    #d = constants.R * c
+    #return d
+
 
   def print(self):
     super().print()
+    print('Neighbors: ', end='')
+    print(self.neighbors)
+    print('Distance: ' + str(self.edge_dist_tt))
