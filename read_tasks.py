232860c94359c1cf56634bc8a5a88f35bd93a9bc
jacobleeliu <44461831+jacobleeliu@users.noreply.github.com>
2020-03-30 19:47:18 -0400

    Add files via upload

---
+++ read_tasks.py
@@ -0,0 +1,84 @@
+#==========================================
+# Title:  Test Station Class
+# Author: NYUPICMathSubwayGroup
+# Date:   2020.03.02
+# Comment:Need pandas to run this test
+#==========================================
+
+import numpy as np
+import pandas as pd
+import argparse
+from station import Station
+from gate import Gate
+from subway_graph import Graph
+import constants
+
+
+def read(path):
+  print(args.file_loc)
+  sheet = pd.read_excel(path[0])
+  station_loc = pd.read_csv(path[1])
+  rows = len(sheet)
+  wkd_graph = Graph(constants.DAY[0])
+  sat_graph = Graph(constants.DAY[1])
+  sun_graph = Graph(constants.DAY[2])
+  #location for stations
+  location_book = {}
+  # build a map for stations and locations
+  for i in range(len(station_loc)):
+    station = station_loc.loc[i, 'Stop Name']
+    latitude = station_loc.loc[i, 'GTFS Latitude']
+    longitude = station_loc.loc[i, 'GTFS Longitude']
+    location_book[station] = [latitude, longitude]
+
+
+
+  for i in range(0, rows):
+    gate_id = sheet.values[i, 1]
+    day = sheet.values[i, 2]
+    # begin_time has 24-hour format in form x00 where it actually means x:00
+    # mod 24 because there are some 2400 to 2500 tasks
+    begin_time = int(int(sheet.values[i, 3]) / 100 % 24)
+    boro = sheet.values[i, 6]
+    routes_str = str(sheet.values[i, 7])
+    name = str(sheet.values[i, 8])
+    routes = routes_str.split(',')
+    task_matrix = np.zeros((24*12, 1))
+    begin_entry = 12 * begin_time
+    comments = sheet.values[i, 10]
+    # mark the matrix
+    for i in range(begin_entry, begin_entry+12):
+      task_matrix[i, 0] = 1
+    # get location of a station
+    loc = location_book.get(name)
+    if not loc:
+      loc = []
+
+    task = Gate(name = name, boro = boro, loc = loc, routes = routes, gate_id = gate_id, begin_time = begin_time,
+                task_matrix = task_matrix, day = day, comments = comments)
+
+    # adding vertex to graph
+    if task.day == constants.DAY[0]:
+      wkd_graph.add_vertex(task)
+    elif task.day == constants.DAY[1]:
+      sat_graph.add_vertex(task)
+    elif task.day == constants.DAY[2]:
+      sun_graph.add_vertex(task)
+
+  #print and see the graphs
+  wkd_graph.print()
+  sat_graph.print()
+  sun_graph.print()
+  #wkd_graph.vertices[0][1].print()
+
+  return wkd_graph, sat_graph, sun_graph
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f', '--file_loc', default='NYCT FE Required Data/SFE SAMPLE210.xlsx', type=str)
+  parser.add_argument('-s', '--station_loc', default='NYCT FE Required Data/station_location.csv', type=str)
+  args = parser.parse_args()
+  path = [args.file_loc, args.station_loc]
+  read(path)
+  
---
+++ subway_graph.py
@@ -0,0 +1,77 @@
+#==========================================
+# Title:  Reading into graph
+# Author: NYUPICMathSubwayGroup
+# Date:   2020.03.02
+# Comment:
+#==========================================
+
+import numpy as np
+from gate import Gate
+import constants
+import copy as cp
+
+class Graph():
+
+  def __init__(self, day = constants.DAY[0]):
+    self.day = day
+    self.empty_vertex = Gate(name = 'Skip', begin_time = 0, day = self.day)
+    # list of lists of tasks at all 24 hours
+    self.vertices = []
+    for i in range(24):
+      self.vertices.append([])
+      temp_empty_vertex = Gate(name = 'Skip', begin_time = i, day = self.day)
+      self.vertices[i].append(temp_empty_vertex)
+      # connect skipping vertex at current layer to that at next layer
+      if i != 0:
+        self.vertices[i-1][0].neighbors.append(temp_empty_vertex)
+        self.vertices[i-1][0].edge_dist_tt.append(0)
+      # connect starting empty vertex with skipping vertex at the first layer
+      else:
+        self.empty_vertex.neighbors.append(temp_empty_vertex) 
+        self.empty_vertex.edge_dist_tt.append(0)
+
+  def add_vertex(self, vertex):
+    # the vertex should be an instance of Gate
+    if type(vertex) != Gate:
+      print('Input vertex should be an instance of Gate')
+      raise Exception('Input vertex should be an instance of Gate')
+
+    time = vertex.begin_time
+    self.vertices[time].append(vertex)
+    if time == 0:
+      # connect starting empty vertex with this vertex
+      self.empty_vertex.neighbors.append(vertex)
+      self.empty_vertex.edge_dist_tt.append(0)
+    else:
+      # connect vertices on last layer and this vertex
+      for prev_vertex in self.vertices[time-1]:
+        prev_vertex.neighbors.append(vertex)
+        # set this to be 5 for now
+        if prev_vertex.name == 'Skip':
+          prev_vertex.edge_dist_tt.append(0)
+        else:
+          distance = prev_vertex.calc_edge_dist_tt(vertex)
+          prev_vertex.edge_dist_tt.append(distance)
+        
+    if time != 23:
+      # connect this vertex and vertices on next layer
+      for next_vertex in self.vertices[time+1]:
+        vertex.neighbors.append(next_vertex)
+        if next_vertex.name == 'Skip':
+          vertex.edge_dist_tt.append(0)
+        else:
+          distance = vertex.calc_edge_dist_tt(next_vertex)
+          vertex.edge_dist_tt.append(distance)
+
+  def print(self):
+    print('Graph for ' + self.day)
+    for lists in self.vertices:
+      print('Begin Time: ' + str(lists[0].begin_time) + '.\tTasks: [', end='')
+      for gate in lists:
+        if gate != lists[len(lists)-1]:
+          print(gate.name, end = ', ')
+        else:
+          print(gate.name, end = '')
+      print(']')
+    print('End Graph for ' + self.day)
+
